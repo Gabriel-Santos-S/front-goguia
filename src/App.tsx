e312473ba55c69e92d7ecd38bafaf119cbe1d6a0
Eduarda Bernardino <eduardabernardinoo06@gmail.com>
2026-05-30 18:44:31 -0300

    feat: adiciona passeios rurais e corrige importacao de imagens

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,37 @@ import Login from './pages/Login';
 import EsqueciSenha from './pages/EsqueciSenha';
 import ConfirmacaoEmail from './pages/Confirmacaoemail'; // Ajustado para o nome que você salvou
 import Home from './pages/home'; // Ajustado para o nome que você salvou
+import RoteirosScreen from './pages/Roteiros';
+
+// function App() {
+  // return (
+    // <BrowserRouter>
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/esqueci-senha" element={<EsqueciSenha />} /> */}
+        {/* <Route path="/confirmacao-email" element={<ConfirmacaoEmail />} /> */}
+        {/* <Route path="/home" element={<Home />} /> */}
+      {/* </Routes> */}
+    {/* </BrowserRouter> */}
+  // );
+// }
+//
+// export default App;
+//
+//
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/esqueci-senha" element={<EsqueciSenha />} />
-        <Route path="/confirmacao-email" element={<ConfirmacaoEmail />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen bg-[#f4f4f6]">
+      {/* Renderiza a tela diretamente para teste */}
+      <RoteirosScreen />
+    </div>
   );
 }
 
 export default App;
+
+
+
