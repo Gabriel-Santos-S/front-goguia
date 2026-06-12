18af71735f042f4f0f68c53ea3e65e956852bd50
Leastro <leastrolgs@gmail.com>
2026-06-12 00:36:11 -0300

    feat: adição de telas. Meus Roteiros, Favoritos, Configurações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import EsqueciSenha from './pages/EsqueciSenha';
-import ConfirmacaoEmail from './pages/Confirmacaoemail'; // Ajustado para o nome que você salvou
-import Home from './pages/home'; // Ajustado para o nome que você salvou
+import ConfirmacaoEmail from './pages/Confirmacaoemail';
+import Home from './pages/home';
 import TelaAvaliacao from './pages/TelaAvaliacao';
 import Agendamentos from './pages/Agendamentos';
 import Pagamento from './pages/Pagamento';
+import MeusRoteiros from './pages/MeusRoteiros';
+import Favoritos from './pages/Favoritos';
+import Configuracoes from './pages/Configuracoes';
 
 function App() {
   return (
@@ -19,9 +22,11 @@ function App() {
         <Route path="/home" element={<Home />} />
         {/* <Route path="/home/tela-avaliacao" element={<TelaAvaliacao />} />
         <Route path="/home/tela-avaliacao/detalhes" element={<Agendamentos />} />
-
         <Route path="/home/pagamento" element={<Pagamento />} /> */}
 
+        <Route path="/meus-roteiros" element={<MeusRoteiros />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+        <Route path="/configuracoes" element={<Configuracoes />} />
       </Routes>
     </BrowserRouter>
   );
